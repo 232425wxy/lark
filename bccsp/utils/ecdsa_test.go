fcb5130ace680cedb773acbae77196f0259febb7
232425wxy <1378406814@mail.nwpu.edu.cn>
2023-03-21 08:43:17 +0800

    ECDSA的测试文件

--- bccsp/utils/ecdsa_test.go
+++ bccsp/utils/ecdsa_test.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"github.com/stretchr/testify/require"
+	"math/big"
 	"testing"
 )
 
@@ -9,4 +10,43 @@ func TestUnmarshalECDSASignature(t *testing.T) {
 	_, _, err := UnmarshalECDSASignature(nil)
 	require.Error(t, err)
 	require.Contains(t, err.Error(), "failed unmarshalling signature [")
+
+	_, _, err = UnmarshalECDSASignature([]byte{})
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "failed unmarshalling signature [")
+
+	_, _, err = UnmarshalECDSASignature([]byte{0})
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "failed unmarshalling signature [")
+
+	sig, err := MarshalECDSASignature(big.NewInt(-1), big.NewInt(1))
+	require.NoError(t, err)
+	_, _, err = UnmarshalECDSASignature(sig)
+	require.Error(t, err)
+	require.Contains(t, err, "invalid signature, R must be larger than 0")
+
+	sig, err = MarshalECDSASignature(big.NewInt(0), big.NewInt(1))
+	require.NoError(t, err)
+	_, _, err = UnmarshalECDSASignature(sig)
+	require.Error(t, err)
+	require.Contains(t, err, "invalid signature, R must be larger than 0")
+
+	sig, err = MarshalECDSASignature(big.NewInt(1), big.NewInt(0))
+	require.NoError(t, err)
+	_, _, err = UnmarshalECDSASignature(sig)
+	require.Error(t, err)
+	require.Contains(t, err, "invalid signature, S must be larger than 0")
+
+	sig, err = MarshalECDSASignature(big.NewInt(1), big.NewInt(-1))
+	require.NoError(t, err)
+	_, _, err = UnmarshalECDSASignature(sig)
+	require.Error(t, err)
+	require.Contains(t, err, "invalid signature, S must be larger than 0")
+
+	sig, err = MarshalECDSASignature(big.NewInt(1), big.NewInt(1))
+	require.NoError(t, err)
+	R, S, err := UnmarshalECDSASignature(sig)
+	require.NoError(t, err)
+	require.Equal(t, R, big.NewInt(1))
+	require.Equal(t, S, big.NewInt(1))
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/232425wxy/lark
 
-go 1.20
+go 1.19
 
 require github.com/stretchr/testify v1.8.2
 
