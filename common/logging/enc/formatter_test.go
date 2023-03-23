cf9204e171deb0aa833f5de917cf30820c9548e8
232425wxy <1378406814@mail.nwpu.edu.cn>
2023-03-23 09:58:51 +0800

    日志格式化的测试案例

--- common/logging/enc/formatter_test.go
+++ common/logging/enc/formatter_test.go
@@ -3,11 +3,15 @@ package enc
 import (
 	"bytes"
 	"fmt"
+	"runtime"
+	"strconv"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap"
 )
 
 func TestParseFormat(t *testing.T) {
@@ -150,5 +154,158 @@ func TestColorFormatter(t *testing.T) {
 			test.f.Format(buf, entry, nil)
 			require.Equal(t, test.formatted, buf.String())
 		})
+	}
+}
+
+func TestLevelFormatter(t *testing.T) {
+	var tests = []struct {
+		level     zapcore.Level
+		formatted string
+	}{
+		{level: zapcore.DebugLevel, formatted: "DEBUG"},
+		{level: zapcore.InfoLevel, formatted: "INFO"},
+		{level: zapcore.WarnLevel, formatted: "WARN"},
+		{level: zapcore.ErrorLevel, formatted: "ERROR"},
+		{level: zapcore.DPanicLevel, formatted: "DPANIC"},
+		{level: zapcore.PanicLevel, formatted: "PANIC"},
+		{level: zapcore.FatalLevel, formatted: "FATAL"},
+		{level: zapcore.Level(99), formatted: "LEVEL(99)"},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("test#%d", i), func(t *testing.T) {
+			buf := &bytes.Buffer{}
+			entry := zapcore.Entry{Level: test.level}
+			LevelFormatter{FormatVerb: "%s"}.Format(buf, entry, nil)
+			require.Equal(t, buf.String(), test.formatted)
+		})
+	}
+}
+
+func TestMessageFormatter(t *testing.T) {
+	buf := &bytes.Buffer{}
+	entry := zapcore.Entry{Message: "some message text \n\n"}
+	f := MessageFormatter{FormatVerb: "%s"}
+	f.Format(buf, entry, nil)
+	require.Equal(t, buf.String(), "some message text ")
+}
+
+func TestModuleFormatter(t *testing.T) {
+	buf := &bytes.Buffer{}
+	entry := zapcore.Entry{LoggerName: "logger/name"}
+	f := ModuleFormatter{FormatVerb: "%s"}
+	f.Format(buf, entry, nil)
+	require.Equal(t, "logger/name", buf.String())
+}
+
+func TestSequenceFormatter(t *testing.T) {
+	mutex := &sync.Mutex{}
+	results := map[string]struct{}{}
+
+	ready := &sync.WaitGroup{}
+	ready.Add(100)
+
+	finished := &sync.WaitGroup{}
+	finished.Add(100)
+
+	SetSequence(0)
+
+	for i := 1; i <= 100; i++ {
+		go func(i int) {
+			buf := &bytes.Buffer{}
+			entry := zapcore.Entry{Level: zapcore.DebugLevel}
+			f := SequenceFormatter{FormatVerb: "%d"}
+			ready.Done() // setup complete
+			ready.Wait() // wait for all go routines to be ready
+
+			f.Format(buf, entry, nil) // format concurrently
+
+			mutex.Lock()
+			results[buf.String()] = struct{}{}
+			mutex.Unlock()
+
+			finished.Done()
+		}(i)
+	}
+
+	finished.Wait()
+	for i := 1; i <= 100; i++ {
+		require.Contains(t, results, strconv.Itoa(i))
+	}
+}
+
+func TestShortFuncFormatter(t *testing.T) {
+	callerpc, _, _, ok := runtime.Caller(0)
+	require.True(t, ok)
+	buf := &bytes.Buffer{}
+	entry := zapcore.Entry{Caller: zapcore.EntryCaller{PC: callerpc}}
+	ShortFuncFormatter{FormatVerb: "%s"}.Format(buf, entry, nil)
+	require.Equal(t, "TestShortFuncFormatter", buf.String())
+
+	buf.Reset()
+	entry = zapcore.Entry{Caller: zapcore.EntryCaller{PC: 0}}
+	ShortFuncFormatter{FormatVerb: "%s"}.Format(buf, entry, nil)
+	require.Equal(t, "unknown", buf.String())
+}
+
+func TestTimeFormatter(t *testing.T) {
+	buf := &bytes.Buffer{}
+	entry := zapcore.Entry{Time: time.Date(2023, time.March, 23, 9, 40, 59, 333, time.Local)}
+	f := TimeFormatter{Layout: time.RFC3339Nano}
+	f.Format(buf, entry, nil)
+	require.Equal(t, "2023-03-23T09:40:59.000000333+08:00", buf.String())
+}
+
+func TestMultiFormatter(t *testing.T) {
+	entry := zapcore.Entry{
+		Message: "message",
+		Level:   zapcore.InfoLevel,
+	}
+	fields := []zapcore.Field{zap.String("key", "value")}
+
+	var tests = []struct{
+		desc string
+		initial []Formatter
+		update []Formatter
+		expected string
+	}{
+		{
+			desc: "no formatters",
+			initial: nil,
+			update: nil,
+			expected: "",
+		},
+		{
+			desc: "initial formatters",
+			initial: []Formatter{StringFormatter{Value: "string1"}},
+			update: nil,
+			expected: "string1",
+		},
+		{
+			desc: "set to formatters",
+			initial: []Formatter{StringFormatter{Value: "string1"}},
+			update: []Formatter{
+				StringFormatter{Value: "string1"},
+				StringFormatter{Value: "-"},
+				StringFormatter{Value: "string2"},
+			},
+			expected: "string1-string2",
+		},
+		{
+			desc: "set to empty",
+			initial: []Formatter{StringFormatter{Value: "string1"}},
+			update: []Formatter{},
+			expected: "",
+		},
+	}
+
+	for _, test := range tests {
+		mf := NewMultiFormatters(test.initial...)
+		if test.update != nil {
+			mf.SetFormatters(test.update)
+		}
+		buf := &bytes.Buffer{}
+		mf.Format(buf, entry, fields)
+		require.Equal(t, test.expected, buf.String())
 	}
 }
